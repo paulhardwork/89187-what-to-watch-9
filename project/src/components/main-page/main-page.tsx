a173c31a24788e68407606b0f8863fc9155086b7
Paul Yarin <pavyarin@yandex.ru>
2022-03-01 23:21:55 +0700

    Добавлены отдельные компоненты для логотипа и подвала сайта

--- project/src/components/main-page/main-page.tsx
+++ project/src/components/main-page/main-page.tsx
@@ -1,4 +1,6 @@
 import FilmCard from '../film-card/film-card';
+import Footer from '../footer/footer';
+import Logo from '../logo/logo';
 
 type MainPageProps = {
   title: string;
@@ -17,13 +19,7 @@ function MainPage({title, genre, releaseDate}: MainPageProps): JSX.Element {
         <h1 className="visually-hidden">WTW</h1>
 
         <header className="page-header film-card__head">
-          <div className="logo">
-            <a className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+          <Logo />
 
           <ul className="user-block">
             <li className="user-block__item">
@@ -134,19 +130,7 @@ function MainPage({title, genre, releaseDate}: MainPageProps): JSX.Element {
           </div>
         </section>
 
-        <footer className="page-footer">
-          <div className="logo">
-            <a className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </>
   );
