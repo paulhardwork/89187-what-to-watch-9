e35cc82a04e540517d6670af11c0d115742eb2ea
Paul Yarin <pavyarin@yandex.ru>
2022-03-19 21:39:53 +0700

    Моки приведены в итоговый вид

--- project/src/components/my-list/my-list.tsx
+++ project/src/components/my-list/my-list.tsx
@@ -33,17 +33,19 @@ function MyList (props: MyListProps): JSX.Element {
         <h2 className="catalog__title visually-hidden">Catalog</h2>
 
         <div className="catalog__films-list">
-          {films.map(({id, name, previewImage}) =>
-            (
-              <article key={id} className="small-film-card catalog__films-card">
-                <div className="small-film-card__image">
-                  <img src={previewImage} alt={name} width="280" height="175" />
-                </div>
-                <h3 className="small-film-card__title">
-                  <Link to={`film/${id}`} className="small-film-card__link">{name}</Link>
-                </h3>
-              </article>
-            ))}
+          {films
+            .filter((film) => film.isFavorite === true)
+            .map(({id, name, previewImage}) =>
+              (
+                <article key={id} className="small-film-card catalog__films-card">
+                  <div className="small-film-card__image">
+                    <img src={previewImage} alt={name} width="280" height="175" />
+                  </div>
+                  <h3 className="small-film-card__title">
+                    <Link to={`film/${id}`} className="small-film-card__link">{name}</Link>
+                  </h3>
+                </article>
+              ))}
         </div>
       </section>
 
