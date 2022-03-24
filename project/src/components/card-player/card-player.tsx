df6ec0226b1c1ef56a2b4eb33310fad03402d74e
Paul Yarin <pavyarin@yandex.ru>
2022-03-25 01:20:21 +0700

    Реализован показ видео при наведении на карточку фильма

--- project/src/components/card-player/card-player.tsx
+++ project/src/components/card-player/card-player.tsx
@@ -1,13 +1,35 @@
+import {useEffect, useRef} from 'react';
+
 type CardPlayerProps = {
   src: string,
   poster: string,
+  mouseOver: boolean,
 }
 
 function CardPlayer (props: CardPlayerProps) {
-  const {src, poster} = props;
+  const {mouseOver, src, poster} = props;
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (videoRef.current === null) {
+      return;
+    }
+
+    if (mouseOver) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.load();
+    }
+  }, [mouseOver]);
 
   return (
-    <video src={src} muted poster={poster}></video>
+    <video
+      ref={videoRef}
+      src={src}
+      muted
+      poster={poster}
+    >
+    </video>
   );
 }
 
--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,19 +1,33 @@
+import { useState } from 'react';
 import {Link} from 'react-router-dom';
 import {Film} from '../../types/film';
+import CardPlayer from '../card-player/card-player';
+
 
 type FilmCardProps = {
   film: Film,
   onMouseOver: () => void,
 };
 
-function FilmCard(props: FilmCardProps): JSX.Element {
+function FilmCard (props: FilmCardProps): JSX.Element {
   const {film, onMouseOver} = props;
-  const {id, name, previewImage} = film;
+  const {id, name, previewImage, previewVideoLink} = film;
+
+  const [isMouseOver, setIsMouseOver] = useState(false);
 
   return (
-    <article onMouseOver={onMouseOver} className="small-film-card catalog__films-card">
+    <article
+      onMouseOver={() => {
+        setIsMouseOver(true);
+        onMouseOver();
+      }}
+      onMouseOut={() => {
+        setIsMouseOver(false);
+      }}
+      className="small-film-card catalog__films-card"
+    >
       <div className="small-film-card__image">
-        <img src={previewImage} alt={name} width="280" height="175" />
+        <CardPlayer mouseOver={isMouseOver} src={previewVideoLink} poster={previewImage} />
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`film/${id}`}>{name}</Link>
